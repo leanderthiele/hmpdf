1835c9f96c6479e4df616c3d0bcac6fc83a41f71
Leander Thiele <leander97@gmx.de>
2020-06-20 16:12:16 -0400

    added the html files again

---
+++ docs/html/a00011.js
@@ -0,0 +1,6 @@
+var a00011 =
+[
+    [ "hmpdf_obj", "a00011.html#a439998de2d821dfe3a21a91fb2687eac", null ],
+    [ "hmpdf_new", "a00011.html#ab087598ef780813528136bcde80deed2", null ],
+    [ "hmpdf_delete", "a00011.html#a9eab1fe0c3c91747a3985839a65e164e", null ]
+];
---
+++ docs/html/files_dup.js
@@ -0,0 +1,11 @@
+var files_dup =
+[
+    [ "hmpdf.h", "a00002.html", null ],
+    [ "hmpdf_configs.h", "a00005.html", "a00005" ],
+    [ "hmpdf_covariance.h", "a00008.html", "a00008" ],
+    [ "hmpdf_data.h", "a00011.html", "a00011" ],
+    [ "hmpdf_init.h", "a00014.html", "a00014" ],
+    [ "hmpdf_onepoint.h", "a00017.html", "a00017" ],
+    [ "hmpdf_powerspectrum.h", "a00020.html", "a00020" ],
+    [ "hmpdf_twopoint.h", "a00023.html", "a00023" ]
+];
