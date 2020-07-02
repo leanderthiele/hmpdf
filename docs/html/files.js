4fc4e2d225a1ca49929a22445905412497674dfc
Leander Thiele <lthiele@princeton.edu>
2020-07-01 22:54:35 -0400

    updated docs

--- docs/html/files.js
+++ docs/html/files.js
@@ -1,14 +1,14 @@
 var files =
 [
-    [ "hmpdf.h", "a00023.html", null ],
-    [ "hmpdf.py", "a00047.html", [
-      [ "HMPDF", "a00055.html", "a00055" ]
+    [ "hmpdf.h", "a00020.html", null ],
+    [ "hmpdf.py", "a00044.html", [
+      [ "HMPDF", "a00052.html", "a00052" ]
     ] ],
-    [ "hmpdf_configs.h", "a00026.html", "a00026" ],
-    [ "hmpdf_covariance.h", "a00029.html", "a00029" ],
-    [ "hmpdf_init.h", "a00032.html", "a00032" ],
-    [ "hmpdf_object.h", "a00035.html", "a00035" ],
-    [ "hmpdf_onepoint.h", "a00038.html", "a00038" ],
-    [ "hmpdf_powerspectrum.h", "a00041.html", "a00041" ],
-    [ "hmpdf_twopoint.h", "a00044.html", "a00044" ]
+    [ "hmpdf_configs.h", "a00023.html", "a00023" ],
+    [ "hmpdf_covariance.h", "a00026.html", "a00026" ],
+    [ "hmpdf_init.h", "a00029.html", "a00029" ],
+    [ "hmpdf_object.h", "a00032.html", "a00032" ],
+    [ "hmpdf_onepoint.h", "a00035.html", "a00035" ],
+    [ "hmpdf_powerspectrum.h", "a00038.html", "a00038" ],
+    [ "hmpdf_twopoint.h", "a00041.html", "a00041" ]
 ];
--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -20,27 +20,5 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 @licend  The above is the entire license notice
 for the JavaScript code in this file
 */
-var NAVTREE =
-[
-  [ "hmpdf", "index.html", [
-    [ "Overview", "index.html", [
-      [ "Introduction", "index.html#intro", null ],
-      [ "Building", "index.html#build", null ],
-      [ "Usage", "index.html#use", null ],
-      [ "Options", "index.html#opt", null ],
-      [ "Runtime", "index.html#time", null ],
-      [ "Error handling", "index.html#errrors", null ],
-      [ "Thread safety", "index.html#threads", null ],
-      [ "Examples", "index.html#examples", null ]
-    ] ],
-    [ "File List", "files.html", "files" ]
-  ] ]
-];
-
-var NAVTREEINDEX =
-[
-"a00020.html"
-];
-
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';
+var menudata={children:[
+{text:"Main Page",url:"index.html"}]}
