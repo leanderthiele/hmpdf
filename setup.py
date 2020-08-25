d89affbc48476c92886e504ab32914d2a24cf8de
Leander Thiele <lthiele@princeton.edu>
2020-08-25 16:41:48 +0200

    more standard python setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hmpdf',
+      description='One- and two-point PDFs within the halo model',
+      version='1',
+      packages=['hmpdf'],
+      python_requires='>3',
+      url='https://github.com/leanderthiele/hmpdf')
