a612848aa7d0e71c8003ff024cc0099850a8767c
Leander Thiele <lthiele@princeton.edu>
2020-08-25 15:35:35 +0200

    added new docs html files

---
+++ docs/html/a00053.js
@@ -0,0 +1,14 @@
+var a00053 =
+[
+    [ "__init__", "a00053.html#ae64f0875afe3067b97ba370b354b9213", null ],
+    [ "__del__", "a00053.html#a41a65d7030dd1006b177d0bc24e1a12b", null ],
+    [ "__enter__", "a00053.html#a404692262c90487a8013dbdd128cdd7f", null ],
+    [ "__exit__", "a00053.html#ac1f38d88d3ea788e8ff4a14467871c54", null ],
+    [ "init", "a00053.html#a9bb50da18f8a0a0a7e938c0b77174826", null ],
+    [ "get_op", "a00053.html#a867e84c6db472dec22c6ddf74d3bf32a", null ],
+    [ "get_tp", "a00053.html#a0af8173d440d53d1c9e018f0fd5d3a13", null ],
+    [ "get_cov", "a00053.html#a0ed0a38e58e7ac325e7537696f96dc13", null ],
+    [ "get_Cell", "a00053.html#adfecc629b5fdd2003a1389b9afbdf530", null ],
+    [ "get_Cphi", "a00053.html#a1c72b11aca95e39b039f2c4807d98956", null ],
+    [ "get_cov_diagnostics", "a00053.html#adeb9ae69b40878c972111d82d79b09c4", null ]
+];
