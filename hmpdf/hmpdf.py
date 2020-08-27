d8d278b8afffcea12955b700d9bbd62008d185ee
Leander Thiele <lthiele@princeton.edu>
2020-08-27 23:14:45 +0200

    cleaned the python wrapper

--- hmpdf/hmpdf.py
+++ hmpdf/hmpdf.py
@@ -1,9 +1,9 @@
 ## \file
-from ctypes import *
+import ctypes as ct
 from os.path import join
 import numpy as np
 from numpy.ctypeslib import ndpointer, as_ctypes
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union, Sequence
 from pkg_resources import resource_filename
 
 ## \cond
@@ -13,9 +13,9 @@ class _C(object) : # char pointer
         pass
     def __call__(self, name) :
         if isinstance(name, str) :
-            return c_char_p(name.encode('utf-8'))
+            return ct.c_char_p(name.encode('utf-8'))
         elif isinstance(name, bytes) :
-            return c_char_p(name)
+            return ct.c_char_p(name)
         else :
             raise TypeError('Not a string.')
 #}}}
@@ -25,7 +25,7 @@ class _E(object) : # enumeration class
     def __init__(self, names) :
         self.names = names
     def __call__(self, name) :
-        return c_int(self.names.index(name))
+        return ct.c_int(self.names.index(name))
 #}}}
 
 class _D(object) : # double pointer from numpy array
@@ -40,15 +40,14 @@ class _D(object) : # double pointer from numpy array
             if not arr.shape[0] == self.l :
                 raise TypeError('Expected numpy array to have length %d, '\
                                 'but has length %d'%(self.l, arr.shape[1]))
-        return c_void_p(np.ascontiguousarray(arr).ctypes.data)
+        return ct.c_void_p(np.ascontiguousarray(arr).ctypes.data)
 #}}}
 
 class _F(object) : # function pointer
 #{{{
     def __init__(self, nargs) :
-        args = [c_double, ] * nargs
-#        args.append(ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1))
-        self.fctptr = CFUNCTYPE(c_double, *args)
+        args = [ct.c_double, ] * nargs
+        self.fctptr = ct.CFUNCTYPE(ct.c_double, *args)
     def __call__(self, f) :
         return self.fctptr(f)
 #}}}
@@ -64,23 +63,23 @@ _corr_types = ['onehalo', 'twohalo', 'total']
 
 class _Configs(object) :
 #{{{
-    configs = ['N_threads', c_int, 'verbosity', c_int, 'warn_is_err', c_int,
+    configs = ['N_threads', ct.c_int, 'verbosity', ct.c_int, 'warn_is_err', ct.c_int,
                'class_pre', _C(), 
-               'N_z', c_int, 'z_min', c_double, 'z_max', c_double,
-               'N_M', c_int, 'M_min', c_double, 'M_max', c_double,
-               'N_signal', c_long, 'signal_min', c_double, 'signal_max', c_double,
-               'N_theta', c_int, 'rout_scale', c_double, 'rout_rdef', _E(_mdefs),
-               'pixel_side', c_double, 'tophat_radius', c_double, 'gaussian_fwhm', c_double,
+               'N_z', ct.c_int, 'z_min', ct.c_double, 'z_max', ct.c_double,
+               'N_M', ct.c_int, 'M_min', ct.c_double, 'M_max', ct.c_double,
+               'N_signal', ct.c_long, 'signal_min', ct.c_double, 'signal_max', ct.c_double,
+               'N_theta', ct.c_int, 'rout_scale', ct.c_double, 'rout_rdef', _E(_mdefs),
+               'pixel_side', ct.c_double, 'tophat_radius', ct.c_double, 'gaussian_fwhm', ct.c_double,
                'custom_ell_filter', _F(1), 'custom_ell_filter_params', None,
                'custom_k_filter', _F(2), 'custom_k_filter_params', None,
-               'N_phi', c_int, 'phi_max', c_double, 'pixelexact_max', c_int,
-               'phi_jitter', c_double, 'phi_pwr', c_double,
-               'zintegr_type', _E(_integr), 'zintegr_alpha', c_double, 'zintegr_beta', c_double,
-               'Mintegr_type', _E(_integr), 'Mintegr_alpha', c_double, 'Mintegr_beta', c_double,
+               'N_phi', ct.c_int, 'phi_max', ct.c_double, 'pixelexact_max', ct.c_int,
+               'phi_jitter', ct.c_double, 'phi_pwr', ct.c_double,
+               'zintegr_type', _E(_integr), 'zintegr_alpha', ct.c_double, 'zintegr_beta', ct.c_double,
+               'Mintegr_type', _E(_integr), 'Mintegr_alpha', ct.c_double, 'Mintegr_beta', ct.c_double,
                'Duffy08_conc_params', _D(9),
                'Tinker10_hmf_params', _D(10),
                'Battaglia12_tsz_params', _D(15),
-               'noise', c_double, ]
+               'noise', ct.c_double, ]
     def __init__(self) :
         self.t = [] # keep references to the types
         self.l = []
@@ -91,11 +90,11 @@ class _Configs(object) :
             raise NotImplementedError('Configuration option %s not '\
                                       'supported in the python wrapper'%key)
         pos = _Configs.configs.index(key)
-        self.l.append(c_int(pos//2))
+        self.l.append(ct.c_int(pos//2))
         self.t.append(_Configs.configs[pos+1])
         self.l.append(self.t[-1](value))
     def __call__(self) :
-        self.l.append(c_int(len(_Configs.configs)//2)) # corresponds to end_configs
+        self.l.append(ct.c_int(len(_Configs.configs)//2)) # corresponds to end_configs
         return self.l
 #}}}
 ## \endcond
@@ -125,55 +124,55 @@ class HMPDF(object) :
     with open(__pathtohmpdf_name, 'r') as f :
         PATHTOHMPDF = f.readline().rstrip()
     try :
-        __libhmpdf = CDLL(join(PATHTOHMPDF, 'libhmpdf.so'))
+        __libhmpdf = ct.CDLL(join(PATHTOHMPDF, 'libhmpdf.so'))
     except OSError : # try to read the shared library from LD_LINKER_PATH
-        __libhmpdf = CDLL('libhmpdf.so')
+        __libhmpdf = ct.CDLL('libhmpdf.so')
     __new = __libhmpdf.hmpdf_new
-    __new.restype = POINTER(c_int)
+    __new.restype = ct.POINTER(ct.c_int)
     __delete = __libhmpdf.hmpdf_delete
-    __delete.restype = c_int
-    __delete.argtypes = [c_void_p, ]
+    __delete.restype = ct.c_int
+    __delete.argtypes = [ct.c_void_p, ]
     __init = __libhmpdf.hmpdf_init_fct
-    __init.restype = c_int
+    __init.restype = ct.c_int
     __get_op = __libhmpdf.hmpdf_get_op
-    __get_op.restype = c_int
-    __get_op.argtypes = [c_void_p, c_int,
-                         ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                         ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                         c_int, c_int, ]
+    __get_op.restype = ct.c_int
+    __get_op.argtypes = [ct.c_void_p, ct.c_int,
+                         ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                         ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                         ct.c_int, ct.c_int, ]
     __get_tp = __libhmpdf.hmpdf_get_tp
-    __get_tp.restype = c_int
-    __get_tp.argtypes = [c_void_p, c_double, c_int,
-                         ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                         ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                         c_int, ]
+    __get_tp.restype = ct.c_int
+    __get_tp.argtypes = [ct.c_void_p, ct.c_double, ct.c_int,
+                         ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                         ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                         ct.c_int, ]
     __get_cov = __libhmpdf.hmpdf_get_cov
-    __get_cov.restype = c_int
-    __get_cov.argtypes = [c_void_p, c_int,
-                          ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                          ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                          c_int, ]
+    __get_cov.restype = ct.c_int
+    __get_cov.argtypes = [ct.c_void_p, ct.c_int,
+                          ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                          ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                          ct.c_int, ]
     __get_Cell = __libhmpdf.hmpdf_get_Cell
-    __get_Cell.restype = c_int
-    __get_Cell.argtypes = [c_void_p, c_int,
-                           ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                           ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                           c_int, ]
+    __get_Cell.restype = ct.c_int
+    __get_Cell.argtypes = [ct.c_void_p, ct.c_int,
+                           ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                           ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                           ct.c_int, ]
     __get_Cphi = __libhmpdf.hmpdf_get_Cphi
-    __get_Cphi.restype = c_int
-    __get_Cphi.argtypes = [c_void_p, c_int,
-                           ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                           ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                           c_int, ]
+    __get_Cphi.restype = ct.c_int
+    __get_Cphi.argtypes = [ct.c_void_p, ct.c_int,
+                           ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                           ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                           ct.c_int, ]
     __get_Nphi = __libhmpdf._get_Nphi
-    __get_Nphi.restype = c_int
-    __get_Nphi.argtypes = [c_void_p, POINTER(c_int), ]
+    __get_Nphi.restype = ct.c_int
+    __get_Nphi.argtypes = [ct.c_void_p, ct.POINTER(ct.c_int), ]
     __get_cov_diagnostics = __libhmpdf.hmpdf_get_cov_diagnostics1
-    __get_cov_diagnostics.restype = c_int
-    __get_cov_diagnostics.argtypes = [c_void_p,
-                                      ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                                      ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1),
-                                      ndpointer(c_double, flags='C_CONTIGUOUS', ndim=1), ]
+    __get_cov_diagnostics.restype = ct.c_int
+    __get_cov_diagnostics.argtypes = [ct.c_void_p,
+                                      ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                                      ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1),
+                                      ndpointer(ct.c_double, flags='C_CONTIGUOUS', ndim=1), ]
     __errmsg = '%s completed with non-zero exit code'
     #}}}
     def __ret(self, err, callname, *args) :
@@ -249,14 +248,14 @@ class HMPDF(object) :
             self.__c.append(k,v)
         arglist = self.__c()
         if stype == 'kappa' :
-            arglist.insert(0, c_double(zsource))
+            arglist.insert(0, ct.c_double(zsource))
         err = HMPDF.__init(self.__d, _C()(class_ini),
                            _E(_stypes)(stype), *arglist)
         return self.__ret(err, 'init()')
     #}}}
 
     def get_op(self,
-               binedges: np.ndarray,
+               binedges: Union[Sequence[float], np.ndarray],
                incl_2h: bool=True,
                noisy: bool=False) -> np.ndarray :
         """! Get the one-point PDF [calls hmpdf_get_op()]
@@ -275,7 +274,7 @@ class HMPDF(object) :
 
     def get_tp(self,
                phi: float,
-               binedges: np.ndarray,
+               binedges: Union[Sequence[float], np.ndarray],
                noisy: bool=False) -> np.ndarray :
         """! Get the two-point PDF [calls hmpdf_get_tp()]
         
@@ -292,7 +291,7 @@ class HMPDF(object) :
     #}}}
 
     def get_cov(self,
-                binedges: np.ndarray,
+                binedges: Union[Sequence[float], np.ndarray],
                 noisy: bool=False) -> np.ndarray :
         """! Get the covariance matrix of the one-point PDF [calls hmpdf_get_cov()]
         
@@ -309,7 +308,7 @@ class HMPDF(object) :
     #}}}
 
     def get_Cell(self,
-                 ell: np.ndarray,
+                 ell: Union[Sequence[float], np.ndarray],
                  mode: str='total') -> np.ndarray :
         """! Get the angular power spectrum [calls hmpdf_get_Cell()]
         
@@ -325,7 +324,7 @@ class HMPDF(object) :
     #}}}
 
     def get_Cphi(self,
-                 phi: np.ndarray,
+                 phi: Union[Sequence[float], np.ndarray],
                  mode: str='total') -> np.ndarray :
         """! Get the angular correlation function [calls hmpdf_get_Cphi()]
         
@@ -346,8 +345,8 @@ class HMPDF(object) :
         @return (phi, phiweights, corr_diagn)
         """
     #{{{
-        Nphi = c_int(0)
-        err = HMPDF.__get_Nphi(self.__d, byref(Nphi))
+        Nphi = ct.c_int(0)
+        err = HMPDF.__get_Nphi(self.__d, ct.byref(Nphi))
         if err :
             return self.__ret(err, 'get_cov_diagnostics()',
                               None, None, None)
