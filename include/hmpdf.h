3a2b8a737e34222a52bd08ded04bee39bfb7fe61
Leander Thiele <lthiele@princeton.edu>
2020-09-07 14:04:45 -0400

    updated docs

--- include/hmpdf.h
+++ include/hmpdf.h
@@ -139,9 +139,8 @@
  *  We can also do this in python:
  *  \snippet example.py example_ell_filter_use
  *
- *  For a more involved example, in examples/forecast/ you can find all ingredients
- *  to reproduce the Fisher forecast from the WL convergence PDF
- *  (see the README there).
+ *  For a more involved example, in examples/WL_PDF_forecast/ you can find all ingredients
+ *  to reproduce the Fisher forecast from the WL convergence PDF.
  */
 
 #include "hmpdf_object.h"
