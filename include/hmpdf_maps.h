26290d8a46d0148811063a015711f5f18998784d
Leander Thiele <lthiele@princeton.edu>
2020-09-22 13:14:17 -0400

    put hmpdf_get_map_ps into header files

--- include/hmpdf_maps.h
+++ include/hmpdf_maps.h
@@ -20,6 +20,22 @@ int hmpdf_get_map_op(hmpdf_obj *d,
                      double op[Nbins],
                      int new_map);
 
+/*! Returns the power spectrum of a simplified simulation (map).
+ *
+ *  \param[in,out] d    hmpdf_init() must have been called on d
+ *  \param[in] Nbins    number of bins the power spectrum will be binned into
+ *  \param[in] binedges monotonically increasing array of length Nbins+1
+ *  \param[out] ps      the binned, direction averaged power spectrum
+ *  \param[in] new_map  if set to non-zero, the simplified simulation will
+ *                      be rerun even if a map has already been generated
+ *  \return error code
+ */
+int hmpdf_get_map_ps(hmpdf_obj *d,
+                     int Nbins,
+                     double binedges[Nbins+1],
+                     double ps[Nbins],
+                     int new_map);
+
 /*! Returns a simplified simulation (map).
  *
  *  \param[in,out] d    hmpdf_init() must have been called on d
